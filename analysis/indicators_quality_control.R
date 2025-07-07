2fd0cace03af84c0c7f561097b7e317eaccc017f
Ileana Marroquin <117444779+ileanamarroquin@users.noreply.github.com>
2025-07-07 15:12:10 -0400

    complete test concept

--- analysis/indicators_quality_control.R
+++ analysis/indicators_quality_control.R
@@ -58,12 +58,13 @@ print(all_mismatched_vars)
 
 # 2. Missing Indicators
 # Here we are interested in flagging those variables expected but not present.
+
 # First, anchor the data frame to the source to subset db_variables to the indicators of
 # interest with the following object:
 
 db_var_meta_source <- db_variables|>
   mutate(variable = case_when(
-    str_starts(variable, "wdi_") ~ "wdi_",
+    str_starts(variable, "wdi_") ~ "wdi_", ### To be updated in the extraction
     TRUE ~ str_extract(variable, "^([^_]+_[^_]+)")
   )) |>
   distinct(variable, source)
@@ -73,6 +74,27 @@ wdi_missing_df <- flag_missing_indicators(db_variables, wdi_indicators, source_t
 
 vdem_missing_df <- flag_missing_indicators(db_variables, vdem_data, source_type = "V-Dem, Variety of Democracy database")
 
+pefa_missing_df <- flag_missing_indicators(db_variables, pefa_assessments, source_type = "Public Expenditure Financial Accountability")
 
 
+# Combine them into a single dataframe
+all_missing_vars <- bind_rows(wdi_missing_df, vdem_missing_df, pefa_missing_df)
+
+
+# test draft --------------------------------------------------------------
+
+if (nrow(all_mismatched_vars) > 0) {
+  message("⚠️ Issue detected: There are mismatched indicators between extracted data and the dictionary.")
+  print(all_mismatched_vars)
+} else {
+  message("✅ All extracted indicators match the dictionary definitions.")
+}
+
+
+if (nrow(all_missing_vars) > 0) {
+  message("⚠️ Issue detected: There are missing indicators not found in the extracted data.")
+  print(all_missing_vars)
+} else {
+  message("✅ All expected indicators are present in the extracted data.")
+}
 
