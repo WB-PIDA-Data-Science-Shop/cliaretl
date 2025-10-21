021c17dce1058b5abddc26c946deb5ce4a0ea9f3
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2025-10-21 08:30:07 -0400

    integrating the pmr data properly from the OECD API and fully documented and reformating the roxygen comments and documentation

--- data-raw/source/pmr.R
+++ data-raw/source/pmr.R
@@ -5,6 +5,7 @@
 library(dplyr)
 library(janitor)
 library(haven)
+library(rsdmx)
 
 ### read in the db variables data
 dbvar_dt <-
@@ -12,34 +13,32 @@ dbvar_dt <-
   filter(source == "OECD Product Market Regulation Database")
 
 
-pmr_df <- haven::read_dta("data-raw/input/pmr/PMR_2018.dta")
+pmr_df <-
+  rsdmx::readSDMX(read.table("data-raw/input/pmr/url.txt")[[1]]) |>
+  as_tibble()
+
+### lets prepare the data we need
+pmr_df <-
+  pmr_df |>
+  pivot_wider(names_from = MEASURE,
+              values_from = obsValue) |>
+  mutate(oecd_pmr_2018_6 = rowMeans(across(c(TARIFFS, FDI_INDEX)), na.rm = TRUE),
+         oecd_pmr_2018_1_3 = PUBLIC_OWNER * 6 / GOVERNANCE,
+         TIME_PERIOD = as.integer(TIME_PERIOD)) |>
+  rename(oecd_pmr_2018_2_2_1 = "INV_BUS_NET",
+         oecd_pmr_2018_2_2_2 = "INV_BUS_SER",
+         oecd_pmr_2018_1_4 = "GOVERNANCE",
+         oecd_pmr_2018_2_1 = "PRICE",
+         oecd_pmr_2018_3_3 = "COMMANDSIMPLIF_BURDEN",
+         country_code = "REF_AREA",
+         year = "TIME_PERIOD") |>
+  dplyr::select(year, country_code, starts_with("oecd_pmr_"))
+
 
 pmr_df <-
   pmr_df |>
-  select(
-    country_code,
-    year,
-    PMR_2018_3_3,
-    PMR_2018_1_3,
-    PMR_2018_6,
-    PMR_2018_1_4,
-    PMR_2018_1_2,
-    PMR_2018_2_1,
-    PMR_2018_1_1,
-    PMR_2018_2_2
-  ) |>
-  clean_names() |>
-  rename_with(
-    # replace prefixes with efi conventions
-    ~ paste0("oecd_", .),
-    .cols = starts_with("pmr")
-  )
-
-
-pmr_df <-
-  pmr_df |>
-  add_plmetadata(source = "",
-                 other_info = "")
+  add_plmetadata(source = "https://www.oecd.org/en/topics/sub-issues/product-market-regulation.html",
+                 other_info = "Pulled from the OECD API url stored in textfile")
 
 pmr <- pmr_df
 
