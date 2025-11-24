23cf402faa6290d3d8982fc9006d6a564959b30d
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2025-11-24 18:11:11 -0500

    update zzz file

--- R/zzz.R
+++ R/zzz.R
@@ -5,5 +5,6 @@ utils::globalVariables(c("country_code", "year", "type_equal",
                          "range_equal", "mean_equal", "median_equal",
                          "missingness_equal", "old_value", "new_value",
                          "over_1pct_change", "is_update", "value_equal",
-                        "abs_diff", "avg_abs_diff"))
+                        "abs_diff", "avg_abs_diff", "abs_diff_pct",
+                        "group_mean"))
 utils::globalVariables(c("avg", "var"))
