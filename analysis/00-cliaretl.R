1ef13e58dcd9e05ca6bfde2c703273c332a817d7
Ileana Marroquin <117444779+ileanamarroquin@users.noreply.github.com>
2025-12-08 21:19:36 -0500

    Version comments

--- analysis/00-cliaretl.R
+++ analysis/00-cliaretl.R
@@ -7,7 +7,13 @@ source(here::here("analysis", "03-ctf_transformations.R"), local = TRUE)
 source(here::here("analysis", "03.1-ctf_quality_control.R"), local = TRUE)
 source(here::here("analysis", "04-map_indicators.R"), local = TRUE)
 
-# Documentation for versioning:
-# https://r-pkgs.org/lifecycle.html#sec-lifecycle-version-number
 
-usethis::use_version("minor")
+# IMPORTANT NOTE ON VERSIONING:
+# This project follows semantic versioning as described in the R Packages book:
+# https://r-pkgs.org/lifecycle.html#sec-lifecycle-version-number
+# Making use of the 'usethis' package to manage versioning:
+# https://usethis.r-lib.org/reference/use_version.html
+
+
+# Increment the minor version for small changes, bug fixes, or documentation updates
+# run the following code in the console to update the version 'usethis::use_version("minor")'
