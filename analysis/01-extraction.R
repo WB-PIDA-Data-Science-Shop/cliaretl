107687be706660162c00b4ef55b831003d90bf20
Galileu Kim <galileukim@worldbank.org>
2026-08-04 11:53:44 -0400

    update etl_source to reflect lazy_load data names

--- analysis/01-extraction.R
+++ analysis/01-extraction.R
@@ -1,8 +1,7 @@
 # Title: Extraction Quality Control Report
 # Description: This script generates a quality control report for the indicators
-# extracted from various databases and updates metadata accordingly.
-
-# Essentially, it checks for mismatched and missing indicators against a reference dictionary.
+# extracted from various databases and updates metadata accordingly. 
+# It checks for mismatched and missing indicators against a reference dictionary.
 
 # - Input:
 # Lazy loaded `data/*.rda` files containing extracted indicators and
@@ -27,19 +26,19 @@ devtools::load_all()
 # read-in data ------------------------------------------------------------
 
 # Extracted indicators dfs
-debt_transparency_indicators <- debt_transparency
-wdi_wb_indicators <- wdi_indicators
-pefa_assessments_indicators <- pefa_assessments
-romelli_indicators <- romelli
-vdem_data_indicators <- vdem_data
-gfdb_indicators <- gfdb
-heritage_indicators <- heritage
-pmr_indicators <- pmr
-epl_indicators <- epl
-d30_indicators <- d360_efi_data
-fraser_indicators <- fraser
-aspire_indicators <- aspire
-wbl_indicators <- wbl_data
+debt_transparency_indicators <- cliaretl::debt_transparency
+wdi_wb_indicators <- cliaretl::wdi_indicators
+pefa_assessments_indicators <- cliaretl::pefa_assessments
+romelli_indicators <- cliaretl::romelli
+vdem_data_indicators <- cliaretl::vdem_data
+gfdb_indicators <- cliaretl::gfdb
+heritage_indicators <- cliaretl::heritage
+pmr_indicators <- cliaretl::pmr
+epl_indicators <- cliaretl::epl
+d30_indicators <- cliaretl::d360_efi_data
+fraser_indicators <- cliaretl::fraser
+aspire_indicators <- cliaretl::aspire
+wbl_indicators <- cliaretl::wbl_data
 
 # Dictionary df
 db_variables_2025 <- readr::read_rds(
@@ -96,7 +95,7 @@ all_mismatched_vars <- map_dfr(
   function(name) {
     mismatched_df <- flag_mismatched_indicators(
       dataframes[[name]],
-      db_variables_2024
+      db_variables_2025
     )[[1]]
     mismatched_df |> mutate(source = name)
   }
@@ -111,7 +110,7 @@ all_mismatched_vars <- map_dfr(
 
 # ... by source -----------------------------------------------------------
 
-dictionary_identifiers <- db_variables_2024 |>
+dictionary_identifiers <- db_variables_2025 |>
   mutate(
     variable = case_when(
       str_starts(variable, "wdi_") ~ "wdi_",
@@ -121,10 +120,9 @@ dictionary_identifiers <- db_variables_2024 |>
   ) |>
   distinct(variable, source)
 
-
 # Then call the function to each of the data frames
 wdi_missing_df <- flag_missing_indicators(
-  db_variables_2024,
+  db_variables_2025,
   wdi_indicators,
   source_type = ("WDI")
 )
@@ -137,61 +135,59 @@ wdi_missing_df <- wdi_missing_df |>
 # This should be empty:
 wdi_missing_df |> filter(!is_present)
 
-
 vdem_missing_df <- flag_missing_indicators(
-  db_variables_2024,
+  db_variables_2025,
   vdem_data_indicators,
   source_type = "V-Dem, Variety of Democracy database"
 )
 
 pefa_missing_df <- flag_missing_indicators(
-  db_variables_2024,
+  db_variables_2025,
   pefa_assessments_indicators,
   source_type = "Public Expenditure Financial Accountability"
 )
 
 pmr_oecd_missing_df <- flag_missing_indicators(
-  db_variables_2024,
+  db_variables_2025,
   pmr_indicators,
   source_type = "OECD Product Market Regulation Database"
 )
 
 epl_oecd_missing_df <- flag_missing_indicators(
-  db_variables_2024,
+  db_variables_2025,
   epl_indicators,
   source_type = "OECD"
 )
 
 gfdb_missing_df <- flag_missing_indicators(
-  db_variables_2024,
+  db_variables_2025,
   gfdb_indicators,
   source_type = "wb_gfdb"
 )
 
 heritage_missing_df <- flag_missing_indicators(
-  db_variables_2024,
+  db_variables_2025,
   heritage_indicators,
   source_type = "Heritage Index of Economic Freedom"
 )
 
 fraser_missing_df <- flag_missing_indicators(
-  db_variables_2024,
+  db_variables_2025,
   fraser_indicators,
   source_type = "Fraser Institute"
 )
 
 aspire_missing_df <- flag_missing_indicators(
-  db_variables_2024,
+  db_variables_2025,
   aspire_indicators,
   source_type = "ASPIRE"
 )
 
 wbl_missing_df <- flag_missing_indicators(
-  db_variables_2024,
+  db_variables_2025,
   wbl_indicators,
   source_type = "CLIAR"
 )
-
 
 # Combine them into a single dataframe
 all_missing_vars <- bind_rows(
@@ -207,7 +203,7 @@ all_missing_vars <- bind_rows(
   wbl_missing_df
 )
 
-
+# flag mismatched indicators
 if (nrow(all_mismatched_vars) > 0) {
   message(
     "⚠️ Issue detected:
@@ -217,7 +213,6 @@ There are mismatched indicators between extracted data and the dictionary."
 } else {
   message("✅ All extracted indicators match the dictionary definitions.")
 }
-
 
 if (nrow(all_missing_vars) > 0) {
   message(
@@ -232,7 +227,7 @@ There are missing indicators not found in the extracted data."
 # ...by etl source -------------------------------------------------------
 # db variables source column is not reliable at this point.
 # Build a compiled indicators panel df to check matching
-sources <- db_variables_2024 |>
+sources <- db_variables_2025 |>
   count(source)
 
 # Step 1: Clean and classify
@@ -262,7 +257,7 @@ dictionary_clean <- dictionary_identifiers |>
           "wb_lpi",
           "wb_wwbi",
           "wjp_rol"
-        ) ~ "wb_api",
+        ) ~ "d360_efi_data",
       variable == "fraser_efw" ~ "fraser",
       variable %in%
         c(
@@ -270,17 +265,17 @@ dictionary_clean <- dictionary_identifiers |>
           "heritage_financial",
           "heritage_investment"
         ) ~ "heritage",
-      variable == "oecd_epl" ~ "oecd_epl",
-      variable == "oecd_pmr" ~ "oecd_pmr",
+      variable == "oecd_epl" ~ "epl",
+      variable == "oecd_pmr" ~ "pmr",
       variable == "romelli_cbi" ~ "romelli",
-      variable == "aspire" ~ "wb_aspire_api",
-      variable == "wb_csc" ~ "wb_csc",
+      variable == "aspire" ~ "aspire",
+      variable == "wb_csc" ~ "scorecard",
       variable == "wb_debt" ~ "debt_transparency",
       variable == "wb_gfdb" ~ "gfdb",
-      variable == "wb_pefa" ~ "pefa",
-      variable == "wb_wbl" ~ "wb_wbl",
-      variable == "vdem_core" ~ "vdem",
-      TRUE ~ "wdi"
+      variable == "wb_pefa" ~ "pefa_assessments",
+      variable == "wb_wbl" ~ "wbl_data",
+      variable == "vdem_core" ~ "vdem_data",
+      TRUE ~ "wdi_indicators"
     )
   )
 
@@ -292,14 +287,14 @@ etl_mapping <- dictionary_clean |>
     source = case_when(
       source == "CLIAR" &
         etl_source == "debt_transparency" ~ "CLIAR (Debt Transparency)",
-      source == "CLIAR" & etl_source == "wb_wbl" ~ "CLIAR (WBL)",
-      source == "CLIAR" & etl_source == "wb_api" ~ "CLIAR (WB API)",
+      source == "CLIAR" & etl_source == "wbl_data" ~ "CLIAR (WBL)",
+      source == "CLIAR" & etl_source == "d360_efi_data" ~ "CLIAR (WB API)",
       T ~ source
     )
   )
 
-# Step 3:Join to db_variables_2024
-db_variables_2025 <- db_variables_2024 |>
+# Step 3:Join to db_variables_2025
+db_variables_2025 <- db_variables_2025 |>
   mutate(
     source = case_when(
       source == "CLIAR" &
